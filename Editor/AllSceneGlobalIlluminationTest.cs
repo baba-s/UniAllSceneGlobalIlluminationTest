d0e4f8d750a4b3705f72990e339c5f40d42a8d91
baba_s <shotababa1206@gmail.com>
2020-09-24 22:04:52 +0900

    first commit

---
+++ Editor/AllSceneGlobalIlluminationTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class AllSceneGlobalIlluminationTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void グローバルイルミネーションが無効になっているか()
+		{
+			AllSceneTester.Test( _ => !Lightmapping.TryGetLightingSettings( out var _ ) );
+		}
+	}
+}
